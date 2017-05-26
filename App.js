e4b577cb23fc0cfde1d7413b2bbcc09577f0f392
shekharj1234 <shekhar.jangde@gmail.com>
2017-05-26 11:12:55 +0530

    basic project setup for Just-Rent

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+           Just developement Start for Just-Rent
+         </div>
+      );
+   }
+}
+export default App;
